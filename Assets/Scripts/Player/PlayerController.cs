6f7b4bd724b0c525e1abff1d76d46d3df72c8191
Kimikhwan <moonfgg@naver.com>
2025-01-31 23:42:25 +0900

    Create Tile Palette

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,68 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float moveInputDirection;
+
+    private bool isFacingRight = true;
+
+    private Rigidbody2D rigid;
+
+    public float moveSpeed = 10.0f;
+    public float jumpForce = 15.0f;
+
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CheckInput();
+        CheckMoveDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMove();
+    }
+
+    private void CheckMoveDirection()
+    {
+        if(isFacingRight && moveInputDirection < 0) 
+        {
+            Flip();
+        }
+        else if(!isFacingRight && moveInputDirection > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void CheckInput()
+    {
+        moveInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigid.linearVelocityY = jumpForce;
+    }
+
+    private void ApplyMove()
+    {
+        rigid.linearVelocityX = moveSpeed * moveInputDirection;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
