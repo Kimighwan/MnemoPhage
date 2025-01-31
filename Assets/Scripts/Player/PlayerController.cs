a55e6c757d5e7d63f017bf4371c7ae9aae5ca730
Kimikhwan <moonfgg@naver.com>
2025-02-01 00:25:11 +0900

    달리기 애니메이션, 바닥 체크로 점프 제한, 방향 전환 Flip, 버그 = 오른쪽으로 달리며 점프가 안 됨(canJump가 false로 됨)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,26 +6,57 @@ public class PlayerController : MonoBehaviour
     private float moveInputDirection;
 
     private bool isFacingRight = true;
+    private bool isRun;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool canJump;
 
     private Rigidbody2D rigid;
+    private Animator anim;
 
+    
     public float moveSpeed = 10.0f;
     public float jumpForce = 15.0f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         CheckInput();
         CheckMoveDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMove();
+        CheckSurroundings();
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rigid.linearVelocityY <= 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump= false;
+        }
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     private void CheckMoveDirection()
@@ -38,6 +69,20 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if(rigid.linearVelocityX != 0)
+        {
+            isRun = true;
+        }
+        else
+        {
+            isRun = false;
+        }
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isRun", isRun);
     }
 
     private void CheckInput()
@@ -52,7 +97,10 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rigid.linearVelocityY = jumpForce;
+        if (canJump)
+        {
+            rigid.linearVelocityY = jumpForce;
+        }
     }
 
     private void ApplyMove()
@@ -65,4 +113,11 @@ public class PlayerController : MonoBehaviour
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
 }
+
