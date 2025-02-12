2b8f3aca3ec8ff8394c47649caccc3c52269b10e
Kimikhwan <moonfgg@naver.com>
2025-02-12 17:28:13 +0900

    UIManager, LobbyUIController, LobbyManager, BaseUI

---
+++ Assets/Scripts/Common/UI/BaseUI.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+public class BaseUIData
+{
+    public Action OnShow;
+    public Action OnClose;
+}
+
+public class BaseUI : MonoBehaviour
+{
+    public Animation uiAnim;    // UI 열 때 애니메이션이 있다면 여기에 할당
+
+    public Action uiOnShow;
+    public Action uiOnClose;
+
+    public virtual void Init(Transform anchor)
+    {
+        Debug.Log($"{GetType()}::SetInit");
+
+        uiOnShow = null;
+        uiOnClose = null;
+
+        transform.SetParent(anchor);
+
+        var rectTransfrom = GetComponent<RectTransform>();
+        rectTransfrom.localPosition = Vector3.zero;
+        rectTransfrom.localScale = Vector3.one;
+        rectTransfrom.offsetMax = Vector3.zero;
+        rectTransfrom.offsetMin = Vector3.zero;
+    }
+
+    public virtual void SetInfo(BaseUIData uiData)
+    {
+        Debug.Log($"{GetType()}::SetInfo");
+
+        uiOnClose = uiData.OnShow;
+        uiOnShow = uiData.OnClose;
+    }
+
+    public virtual void ShowUI()
+    {
+        if(uiAnim != null)
+        {
+            uiAnim.Play();
+        }
+
+        uiOnShow?.Invoke();
+        uiOnShow = null;
+    }
+
+    public virtual void CloseUI(bool isCloseAll = false)
+    {
+        if (!isCloseAll)
+        {
+            uiOnClose?.Invoke();
+        }
+        uiOnClose = null;
+
+        UIManager.Instance.CloseUI(this);
+    }
+
+    public virtual void OnClickCloseButton()
+    {
+        // AudioManager.Instance.PlaySFX(SFX.ui_button_click);
+        CloseUI();
+    }
+}
